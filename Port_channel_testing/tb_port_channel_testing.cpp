19e872367ef30bec173326f5af3faac1bf501a7a
MahishaMurali <mahishaballapura@ufl.edu>
2025-03-02 11:19:49 -0500

    Add files via upload

---
+++ Port_channel_testing/tb_port_channel_testing.cpp
@@ -0,0 +1,17 @@
+#include "systemc.h"
+#include "port_channel_testing.cpp"
+SC_MODULE(testbench){
+
+MODULE1 m1{"m1"};
+MODULE2 m2 {"m2"};
+sc_signal<int> s;
+SC_CTOR(testbench){
+//m1.p(s);
+	m2.p(m1.p);
+}	
+};
+int sc_main(int, char*[]) {
+ testbench tb ("tb");
+  sc_start(10, SC_SEC);
+  return 0;
+}
